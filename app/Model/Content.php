4a254ee00188dca32052c67116d92bf136e732d7
joeldsouza94 <joel.dsouza666@gmail.com>
2017-11-18 19:19:54 +0530

    created relationships

--- app/Model/Content.php
+++ app/Model/Content.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use App\Model\Topic;
 use Illuminate\Database\Eloquent\Model;
 
 class Content extends Model
 {
-    //
+    public function topic() {
+    	return $this->belongsTo(Topic::class);
+    } 
 }
--- app/Model/Topic.php
+++ app/Model/Topic.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use App\Model\Content;
 use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    public function contents() {
+    	return $this->hasMany(Content::class);
+    } 
 }
